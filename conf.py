cb0c801baca6ccd8c314972eaca6135ba82b7d9b
Riccardo Vianello <riccardo.vianello@gmail.com>
2015-07-17 23:49:16 +0200

    reset author to 'l4c'

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "linux4chemistry"  # (translatable)
+BLOG_AUTHOR = "l4c"  # (translatable)
 BLOG_TITLE = "Linux4Chemistry"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link
